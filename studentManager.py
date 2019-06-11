2a8715bdd296cb3655144739b7ce6382e9e1231f
hysspoluo <517645428@qq.com>
2019-06-11 17:31:49 +0800

    2

--- studentManager.py
+++ studentManager.py
@@ -33,6 +33,7 @@ def insert():
         id = input("请输入学生ID:")
         if not id:
             break
+
         name = input("请输入学生姓名:")
         if not name:
             break
@@ -42,13 +43,12 @@ def insert():
             c = int(input("请输入c语言成绩:"))
         except:
             print("请输入正确格式")
-            continue
         #学生信息正确，保存到字典
         student_info = {"id":id,"name":name,"english":english,"python":python,"c":c}
         student_list.append(student_info)
         #添加完一个人了，询问是否继续
         while(1):
-            inputMark = input("请问是否继续添加?(y/n)")
+            inputMark = input("请问是否继续添加?(y/n):")
             if inputMark not in ["y","n"]:
                 print("输入错误")
                 continue
@@ -61,18 +61,150 @@ def insert():
     save(student_list)
     print("信息保存完毕")
 
-
-
-
 #查找学生信息
 def search():
-    return
+    mark = True
+    while mark:
+        studentId = input("请输入需要查询的学生的ID:")
+        if studentId is not "":
+            #开始查询工作
+            if os.path.exists(fileName):
+                with open(fileName,'r') as rfile:
+                    student_old = rfile.readlines()
+            else:
+                student_old = []
+            if student_old:
+                #如果有数据
+                student_search = []
+                for list in student_old:
+                    d= dict(eval(list))
+                    if d["id"] == studentId:
+                        student_search.append(d)
+                show_student(student_search)
+                student_search.clear()#清空列表
+            else:
+                #如果没有数据
+                print("目前没有数据")
+                break
+        else:
+            print("输入ID错误")
+        while (1):
+            inputMark = input("请问是否继续查询?(y/n):")
+            if inputMark not in ["y", "n"]:
+                print("输入错误")
+                continue
+            else:
+                break
+        if inputMark == "y":
+            mark = True
+        else:
+            mark = False
 #删除学生信息
 def delete():
-    return
+    mark = True
+    while mark:
+        studentId = input("请输入要删除的学生的ID:")
+        if studentId is not "":
+            #输入的id有内容
+            if os.path.exists(fileName):
+                with open(fileName,'r') as rfile:
+                    student_old = rfile.readlines()
+            else:
+                student_old=[]
+            ifdel = False#是否删除的标记
+            if student_old:
+                with open(fileName,'w') as wfile:
+                    d= {}
+                    for list in student_old:
+                        d = dict(eval(list))
+                        if d["id"]!=studentId:
+                            #该条记录不用删，重新写入文件,只要正常数据，有逐条重新写入
+                            wfile.write(str(d)+"\n")
+                        else:
+                            ifdel = True
+                if ifdel:
+                    print("该学生信息已经删除")
+                else:
+                    print("不存在该学生信息")
+            else:
+                print("目前没有学生数据，无需删除")
+                break
+        else:
+            print("输入ID错误")
+        #新的判断，看是否继续
+        #显示处理后的结果
+        show()
+        while (1):
+            inputMark = input("请问是否继续删除?(y/n):")
+            if inputMark not in ["y", "n"]:
+                print("输入错误")
+                continue
+            else:
+                break
+        if inputMark == "y":
+            mark = True
+        else:
+            mark = False
+
 #修改学生信息
 def modify():
-    return
+    mark = True
+    while mark:
+        studentId = input("请输入要修改的学生的ID:")
+        if studentId is not "":
+            #开始修改数据
+            if os.path.exists(fileName):
+                with open(fileName,'r') as rfile:
+                    student_old = rfile.readlines()
+            else:
+                student_old = []
+            ifmodify = False  # 是否修改完成
+            if student_old:
+                with open(fileName,'w') as wfile:
+                    d= {}
+                    for list in student_old:
+                        d = dict(eval(list))
+                        if d["id"]!=studentId:
+                            #该条记录不用删，重新写入文件,只要正常数据，有逐条重新写入
+                            wfile.write(str(d)+"\n")
+                        else:
+                           #开始修改记录
+                            ifmodify = True
+                            while(1):
+                                try:
+                                    english = int(input("请输入英语成绩；"))
+                                    python = int(input("请输入python成绩:"))
+                                    c = int(input("请输入c语言成绩:"))
+                                except:
+                                    print("请输入正确格式")
+                                    continue
+
+                                d["english"] = english
+                                d["python"] = python
+                                d["c"]= c
+                                wfile.write(str(d) + "\n")
+                                break
+                if ifmodify:
+                    print("该学生信息已经修改")
+                else:
+                    print("不存在该学生信息")
+
+
+        else:#没有数据
+            print("输入ID错误")
+        while (1):
+            inputMark = input("请问是否继续修改?(y/n):")
+            if inputMark not in ["y", "n"]:
+                print("输入错误")
+                continue
+            else:
+                break
+        if inputMark == "y":
+            mark = True
+        else:
+            mark = False
+
+
 #信息排序
 def sort():
     return
@@ -147,6 +279,7 @@ def main():
             ctrl = False
         if option_int == 1:
             insert()#新建信息
+
         if option_int == 2:
             search()#查询信息
         if option_int == 3:
